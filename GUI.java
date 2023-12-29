fd79b0e88cbff1be1d0db01ca6ff3df8312a1eb9
Leonis Panagiotis <67801870+TheReaperGR@users.noreply.github.com>
2023-12-30 00:16:22 +0200

    Update GUI.java
    it bearly works

--- GUI.java
+++ GUI.java
@@ -2,16 +2,16 @@ import org.apache.lucene.queryparser.classic.ParseException;
 
 import java.awt.*;
 import java.io.IOException;
-import java.util.List;
 import javax.swing.*;
 
 public class GUI extends JFrame {
-
-    private static String userQuery;
+    private static SearchCallback searchCallback;
     private static String selectedMode;
+    private static String userQuery;
+    private static JLabel info;
+    private static JTextArea resultsArea;
 
     public GUI() {
-
         // Set frame properties
         setTitle("My GUI Application");
         setSize(720, 480);
@@ -23,7 +23,7 @@ public class GUI extends JFrame {
 
         Box box = Box.createHorizontalBox();
 
-        panel.setBackground(new Color(36, 36, 36));
+        panel.setBackground(new Color(235, 0, 255));
         // Add space at the top
         JLabel spaceLabel = new JLabel(" ");
         box.add(spaceLabel);
@@ -32,56 +32,45 @@ public class GUI extends JFrame {
         JTextField searchBox = new JTextField(20);
         box.add(searchBox);
 
-        //Get user input search terms:
-        String userQuery = searchBox.getText();
-
         // Add a JComboBox for search modes selection:
-        //NOTE: the plan is to have 4 modes. Lyrics mode, Songs mode, Albums mode and Joker mode (and one of these is not like the
-        //others. Joke mode is a "universal searcher" that looks for the search terms in EVERY category and not just
-        //a specified one.
-        String[] searchModes = {"Lyrics", "Songs", "Albums"};   //, "Joker"
+        String[] searchModes = {"lyrics", "songs", "albums"};
         JComboBox<String> modeSelector = new JComboBox<>(searchModes);
-
         box.add(modeSelector);
 
-        //Get user input search mode:
-        String selectedMode = (String) modeSelector.getSelectedItem();
-
-        //lastly add GO button:
+        // lastly add GO button:
         JButton searchButton = new JButton("Go!");
-        searchButton.setBackground(new Color(16, 136, 26, 255));
+        searchButton.setBackground(new Color(255, 255, 255, 255));
         box.add(searchButton);
-
-        searchButton.addActionListener(e->{
-            try {
-                List<SearchResult> searchResults = Main.regularSearch( getSelectedMode() );
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            } catch (ParseException ex) {
-                throw new RuntimeException(ex);
-            }
+        searchButton.addActionListener(e -> {
+            // Get user input search mode:
+            selectedMode = (String) modeSelector.getSelectedItem();
+            userQuery = searchBox.getText();  // Update userQuery with the actual user input
+            searchCallback.onSearch(selectedMode);
         });
 
         panel.add(box);
 
-        //------------------BOTTOM--SECTION------------------
-
         // Add a line separator
-        addSeparator(panel,100);
+        addSeparator(panel, 100);
 
         // Add a JTextArea for displaying results
-        JTextArea resultsArea = new JTextArea(20, 50);
+        resultsArea = new JTextArea(20, 50);
         resultsArea.setEditable(false); // Make it read-only
         JScrollPane scrollPane = new JScrollPane(resultsArea);
 
-        resultsArea.setBackground(new Color(79, 79, 79));
-
-        resultsArea.setText("you searched for: " + userQuery);
+        resultsArea.setBackground(new Color(75, 255, 0, 255));
 
         panel.add(scrollPane);
         // Add the panel to the frame
         add(panel);
+    }
 
+    public static void appendResultTextArea(String res) {
+        resultsArea.append(res + "\n");
+    }
+
+    public static void appendInfoText(String inf) {
+        info.setText(inf + "\n");
     }
 
     private void addSeparator(JPanel panel, int height) {
@@ -91,7 +80,8 @@ public class GUI extends JFrame {
         panel.add(separator);
     }
 
-    public static void renderGUI() {
+    public static void renderGUI(SearchCallback callback) {
+        searchCallback = callback;
         // Create and show the GUI
         javax.swing.SwingUtilities.invokeLater(() -> {
             GUI gui = new GUI();
@@ -107,4 +97,7 @@ public class GUI extends JFrame {
         return selectedMode;
     }
 
+    public interface SearchCallback {
+        void onSearch(String selectedMode);
+    }
 }
