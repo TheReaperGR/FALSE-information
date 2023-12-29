457773becdbf3b7fac9b5afa95c46f41a61d4a46
Leonis Panagiotis <67801870+TheReaperGR@users.noreply.github.com>
2023-12-30 00:16:01 +0200

    Update Main.java
    it bearly works

--- Main.java
+++ Main.java
@@ -1,78 +1,84 @@
-import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 import org.apache.lucene.queryparser.classic.ParseException;
 
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Map;
 
 public class Main {
-    private static final String INDEX_DIR = "E:\\Intellij IDEA projects\\false_v1\\Data";
-    private static final String DATA_DIR = "E:\\Intellij IDEA projects\\false_v1\\Data\\songsOutput";
-    private static final String ALBUM_DIR = "E:\\Intellij IDEA projects\\false_v1\\Data\\albumOutput";
-    private static final String LYRICS_DIR = "E:\\Intellij IDEA projects\\false_v1\\Data\\lyricsOutput";
-    private static String searchPath;
+    private static final String INDEX_DIR = "C:\\Users\\Reaper\\IdeaProjects\\false\\src\\";
+    private static final String DATA_DIR = "C:\\Users\\Reaper\\IdeaProjects\\false\\src\\songsOutput";
+    private static final String ALBUM_DIR = "C:\\Users\\Reaper\\IdeaProjects\\false\\src\\albumOutput";
+    private static final String LYRICS_DIR = "C:\\Users\\Reaper\\IdeaProjects\\false\\src\\lyricsOutput";
+    private static final Map<String, String> SEARCH_PATHS = new HashMap<>();
+
+    // Additional variables to keep track of the current search type and search path
     private static String searchType;
+    private static String searchPath;
 
     public static void main(String[] args) {
-        //open the GUI window:
-        GUI.renderGUI();
-        searchType = GUI.getSelectedMode();
-
+        // parse all the CSVs
         try {
-            //parse all the CSVs
             Parser.parseAll();
-            //create indeces
-            createIndex(searchType);
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (CsvValidationException | ParseException e) {
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+
+        // open the GUI window:
+        GUI.renderGUI(new GUI.SearchCallback() {
+            @Override
+            public void onSearch(String selectedMode) {
+                searchType = selectedMode;
+                try {
+                    regularSearch(searchType);
+                } catch (IOException | ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        // create indices for all paths
+        try {
+            createAllIndices();
+        } catch (IOException | ParseException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void createIndex(String searchType) throws IOException, ParseException {
+    private static void createAllIndices() throws IOException, ParseException {
         Indexer indexer = new Indexer(INDEX_DIR);
 
         if (indexer.isIndexExists()) {
             System.out.println("Index already exists. Skipping indexing step.");
         } else {
-            int numIndexed = 0;
-            long startTime = System.currentTimeMillis();
-            if (!(indexer.isIndexExists())) {
-                switch (searchType) {
-                    case "lyrics":
-                        numIndexed = indexer.createIndex(LYRICS_DIR, new TextFileFilter());
-                        break;
-                    case "song":
-                        numIndexed = indexer.createIndex(DATA_DIR, new TextFileFilter());
-                        break;
-                    case "album":
-                        numIndexed = indexer.createIndex(ALBUM_DIR, new TextFileFilter());
-                        break;
-                    default:
-                        System.out.println("Invalid search type.");
-                        return;
-                }
-            }
-            long endTime = System.currentTimeMillis();
-            System.out.println(numIndexed + " File(s) indexed, time taken: " +
-                    (endTime - startTime) + " ms");
-        }
+            try {
+                int numIndexed = 0;
+                long startTime = System.currentTimeMillis();
 
-        indexer.close();
+                numIndexed += indexer.createIndex(LYRICS_DIR, new TextFileFilter());
+                numIndexed += indexer.createIndex(DATA_DIR, new TextFileFilter());
+                numIndexed += indexer.createIndex(ALBUM_DIR, new TextFileFilter());
+
+                long endTime = System.currentTimeMillis();
+                System.out.println(numIndexed + " File(s) indexed, time taken: " +
+                        (endTime - startTime) + " ms");
+            } finally {
+                indexer.close(); // Ensure that the indexer is closed even if an exception occurs
+            }
+        }
     }
 
-    public static List<SearchResult> regularSearch(String searchType) throws IOException, ParseException {
-
+    public static void regularSearch(String st) throws IOException, ParseException {
+        searchType = st;
         String searchDirectory = setSearchPath();
 
         Searcher searcher = new Searcher(INDEX_DIR);
         long startTime = System.currentTimeMillis();
 
-        //get user query from gui search-box:
+        // get user query from GUI search-box:
         String query = GUI.getUserQuery();
 
         List<SearchResult> searchResults = searcher.search(query, searchType, searchDirectory);
@@ -81,49 +87,40 @@ public class Main {
         searcher.close();
         System.out.println("Search completed in " + (endTime - startTime) + " ms");
 
-        return searchResults;
-
-        //note: once the list of all results has been created we then return them to the GUI so it can print them out
-        /*
+        // note: once the list of all results has been created we then send them to the GUI so it can print them out
         for (SearchResult result : searchResults) {
-            System.out.println("File Path: " + result.getFilePath());
-            System.out.println("Name: " + result.getName());
-            System.out.println("Artist: " + result.getArtist());
+            GUI.appendResultTextArea("File Path: " + result.getFilePath());
+            GUI.appendResultTextArea("Name: " + result.getName());
+            GUI.appendResultTextArea("Artist: " + result.getArtist());
 
             // Check the search type for specific fields
             if ("song".equals(searchType)) {
-                System.out.println("Album Name: " + result.getAlbumName());
+                GUI.appendResultTextArea("Album Name: " + result.getAlbumName());
             } else if ("album".equals(searchType)) {
-                System.out.println("Year: " + result.getYear());
+                GUI.appendResultTextArea("Year: " + result.getYear());
             }
 
-            System.out.printf("Score: %.2f\n", result.getScore());
-            System.out.println();
+            float score = result.getScore();    // get the score and send it
+            GUI.appendInfoText("Score: " + Float.toString(score));
         }
-
-
-         */
     }
 
-    private static String setSearchPath(){
+    private static String setSearchPath() {
         switch (searchType) {
             case "lyrics":
                 searchPath = LYRICS_DIR;
-
                 break;
-            case "song":
+            case "songs":
                 searchPath = DATA_DIR;
                 break;
-            case "album":
+            case "albums":
                 searchPath = ALBUM_DIR;
                 break;
             default:
                 System.out.println("Invalid search type.");
+                searchPath = ""; // or provide a default value
         }
 
         return searchPath;
     }
-
-
-
 }
